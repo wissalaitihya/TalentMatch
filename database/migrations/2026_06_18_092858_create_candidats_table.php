80e523c1ed57871cd68ef007ee1b36041085a3be
wissalaitihya <wissalaitihya@gmail.com>
2026-06-18 10:56:59 +0100

    create offres , candidats , analyse and messages tabes

---
+++ database/migrations/2026_06_18_092858_create_candidats_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('candidats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('offre_id')->constrained()->cascadeOnDelete();
+            $table->string('nom_candidat');
+            $table->text('cv_texte');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('candidats');
+    }
+};
